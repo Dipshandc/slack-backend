cd03f3feb71c3326ba59920e4603a30e782ab56d
Dipshandc <dipshandcnew@gmail.com>
2024-09-18 20:35:52 +0545

    update:changed url for endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path 
-from .views import FileuploadView, GetChannelsAndUsersView, AccessTokenView, MessageChannelView
+from .views import *
 
 urlpatterns = [
-  path('upload/', FileuploadView.as_view(), name='file_upload'),
-  path('channels/', GetChannelsAndUsersView.as_view(), name='channels'),
-  path('access-token/', AccessTokenView.as_view(), name='access-token'),
+  path('upload/', SlackFileUploadAPIView.as_view(), name='file_upload'),
+  path('channels/', SlackGetChannelsAndUsersAPIView.as_view(), name='channels'),
+  path('access-token/', SlackSaveAccessTokenAPIView.as_view(), name='access-token'),
   path('message/<str:id>', MessageChannelView.as_view(), name='message-channel'),
 ]
