778947e05734711829363696f7f0bc3546424270
Dipshandc <dipshandcnew@gmail.com>
2024-09-23 20:25:11 +0545

    update:updated enpoints

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
   path('upload/', SlackFileUploadAPIView.as_view(), name='file_upload'),
   path('channels/', SlackGetChannelsAndUsersAPIView.as_view(), name='channels'),
   path('access-token/', SlackSaveAccessTokenAPIView.as_view(), name='access-token'),
-  path('message/<str:id>', MessageChannelView.as_view(), name='message-channel'),
+  path('message/<str:id>', SlackSendMessageAPIView.as_view(), name='message-channel'),
   path('disconnect-slack/', SlackDisconnetAPIView.as_view(), name='message-channel'),
   
 ]
